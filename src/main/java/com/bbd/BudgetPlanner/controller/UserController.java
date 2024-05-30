9fe5c2457a2f54e9546dfcb2a5dc4c724b695eeb
devwasabi <159067732+devwasabi@users.noreply.github.com>
2024-05-30 16:51:15 +0200

    Update UserController.java

--- src/main/java/com/bbd/BudgetPlanner/controller/UserController.java
+++ src/main/java/com/bbd/BudgetPlanner/controller/UserController.java
@@ -3,15 +3,15 @@ package com.bbd.BudgetPlanner.controller;
 import java.util.HashMap;
 import java.util.Optional;
 
-//import java.util.Collections;
-//import java.util.Map;
+import java.util.Collections;
+import java.util.Map;
 //
-//import org.springframework.security.core.annotation.AuthenticationPrincipal;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbd.BudgetPlanner.models.Users;
@@ -33,10 +33,10 @@ public class UserController {
         this.repo = repo;
     }
 
-    //@GetMapping("/user")
-    //public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-    //    return Collections.singletonMap("username", principal.getAttribute("username"));
-    //}
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("username", principal.getAttribute("username"));
+    }
 
     @PostMapping("/api/user")
     public ResponseEntity<?> createUser(@RequestBody Users user) {
