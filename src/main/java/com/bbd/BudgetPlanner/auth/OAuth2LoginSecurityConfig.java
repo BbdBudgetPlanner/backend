2c8767c5b2d3c4f698c7b63c8dc2c80c84fb30cc
devwasabi <159067732+devwasabi@users.noreply.github.com>
2024-05-30 16:44:54 +0200

    Update OAuth2LoginSecurityConfig.java

--- src/main/java/com/bbd/BudgetPlanner/auth/OAuth2LoginSecurityConfig.java
+++ src/main/java/com/bbd/BudgetPlanner/auth/OAuth2LoginSecurityConfig.java
@@ -1,23 +1,23 @@
 package com.bbd.BudgetPlanner.auth;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class OAuth2LoginSecurityConfig {
 
-    //@Bean
-    //public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //    http.authorizeHttpRequests(
-    //        (authorize) -> authorize
-    //            .anyRequest().authenticated()
-    //    )
-    //    .oauth2Login(withDefaults());
-    //    return http.build();
-    //}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            (authorize) -> authorize
+                .anyRequest().authenticated()
+        )
+        .oauth2Login(withDefaults());
+        return http.build();
+    }
 }
