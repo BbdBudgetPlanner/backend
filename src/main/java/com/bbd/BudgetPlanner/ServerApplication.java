8618de67ff2fb25b8c76afd22b9a08241c0fc033
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-05-31 09:17:18 +0200

    added dotenv dependency to hide db details

--- src/main/java/com/bbd/BudgetPlanner/ServerApplication.java
+++ src/main/java/com/bbd/BudgetPlanner/ServerApplication.java
@@ -2,6 +2,7 @@ package com.bbd.BudgetPlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 //import org.springframework.boot.autoconfigure.domain.EntityScan;
 //import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.load();
+    System.setProperty("DB_URL", dotenv.get("DB_URL"));
+    System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+    System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
     SpringApplication.run(ServerApplication.class, args);
   }
 }
