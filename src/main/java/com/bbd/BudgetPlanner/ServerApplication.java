712650358722c6a5586584b5404f9399cc22855c
devwasabi <Wandile.Nyembe@bbd.co.za>
2024-06-10 23:57:31 +0200

    changed origin cors url again to https

--- src/main/java/com/bbd/BudgetPlanner/ServerApplication.java
+++ src/main/java/com/bbd/BudgetPlanner/ServerApplication.java
@@ -26,7 +26,7 @@ public class ServerApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-        .allowedOrigins("http://planback.eu-west-1.elasticbeanstalk.com")
+        .allowedOrigins("https://dfn01vp2479p8.cloudfront.net")
         .allowedHeaders("Content-Type", "Authorization")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 			}
